e03790c2a35a2c41ad95f6fd39a2628edff4ffa2
Vivek Pandya <vivek.pandya93@gmail.com>
2014-04-12 23:54:09 -0700

    users

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  #Required for heroku, set to your actual domain name
+
+  config.action_mailer.default_url_options = { :host => 'bruinconnect.com' }
 end
